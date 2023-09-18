002b4fceb1eb752a0fcc5ec1aff9db85fdf715f6
Huzaifa353 <huzefashamoon@gmail.com>
2023-09-18 19:04:27 +0500

    Property Images Added and Image Popup Created

--- src/pages/property/[id].jsx
+++ src/pages/property/[id].jsx
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { Gallery, Item } from "react-photoswipe-gallery";
 import "photoswipe/dist/photoswipe.css";
 import CopyrightFooter from "../../components/common/footer/CopyrightFooter";
@@ -9,7 +9,6 @@ import MobileMenu from "../../components/common/header/MobileMenu";
 import PopupSignInUp from "../../components/common/PopupSignInUp";
 import DetailsContent from "../../components/listing-details-v1/DetailsContent";
 import Sidebar from "../../components/listing-details-v1/Sidebar";
-import Image from "next/image";
 import { fetchProperty } from "../../utils/api";
 import global from "../../config/env";
 
@@ -30,6 +29,34 @@ const PropertyView = ({property}) => {
     minimumFractionDigits: 0,
     maximumFractionDigits: 0,
   });
+
+  const popupRef = useRef(null);
+  const imageRef = useRef(null);
+  const [popupImage, setPopupImage] = useState('');
+  const [showPopup, setShowPopup] = useState(false);
+  useEffect(() => {
+    // Check if we are running on the client side before manipulating the DOM
+    if (typeof window !== 'undefined' && showPopup) {
+      const popup = popupRef.current;
+      const image = imageRef.current;
+
+      // Perform your DOM manipulations here
+      if (popup && image) {
+        // Example: Change the display style of the popup and image
+        popup.style.display = 'flex';
+        image.src = popupImage;
+      }
+    }
+    else {
+      const popup = popupRef.current;
+      popup.style.display = 'none';
+    }
+  }, [showPopup]);
+  const handlePopup = (show, imageName=null) => {
+    if(imageName)
+      setPopupImage(global.apiURL + 'images/' + imageName)
+    setShowPopup(show);
+  };
 
   return (
     <>
@@ -91,29 +118,24 @@ const PropertyView = ({property}) => {
             </div>
             {/* End .row */}
 
-            <div className="row">
+            <div ref={popupRef} id="popup" onClick={() => handlePopup(false)}>
+                <img ref={imageRef} id="popupImage" src="" alt=""/>
+            </div>
+
+            <div className="row property-images">
               <div className="col-sm-7 col-lg-8">
                 <div className="row">
                   <div className="col-lg-12">
                     <div className="spls_style_two mb30-520">
-                      <Item
-                        original={property?.image}
-                        thumbnail={property?.image}
-                        width={752}
-                        height={450}
-                      >
-                        {({ ref, open }) => (
-                          <div role="button" ref={ref} onClick={open}>
-                            <img
-                              width={752}
-                              height={450}
-                              className="img-fluid w100 cover lds-1"
-                              src={global.apiURL + 'images/' + property.image}
-                              alt={property.name}
-                            />
-                          </div>
-                        )}
-                      </Item>
+                        <div role="button" onClick={() => handlePopup(true, property.image)}>
+                          <img
+                            width={752}
+                            height={450}
+                            className="img-fluid w100 cover lds-1"
+                            src={global.apiURL + 'images/' + property.image}
+                            alt={property?.name}
+                          />
+                        </div>
                     </div>
                   </div>
                 </div>
@@ -122,27 +144,18 @@ const PropertyView = ({property}) => {
 
               <div className="col-sm-5 col-lg-4">
                 <div className="row">
-                  {property?.imgList?.map((val, i) => (
+                  {property?.images?.map((image, i) => (
                     <div className="col-6" key={i}>
                       <div className="spls_style_two img-gallery-box mb24">
-                        <Item
-                          original={val}
-                          thumbnail={val}
-                          width={752}
-                          height={450}
-                        >
-                          {({ ref, open }) => (
-                            <div role="button" ref={ref} onClick={open}>
-                              <Image
-                                width={170}
-                                height={133}
-                                className="img-fluid w100 cover"
-                                src={val}
-                                alt="2.jpg"
-                              />
-                            </div>
-                          )}
-                        </Item>
+                        <div role="button" onClick={() => handlePopup(true, image)}>
+                          <img
+                            width={170}
+                            height={133}
+                            className="img-fluid w100 cover"
+                            src={global.apiURL + 'images/' + image}
+                            alt={property?.name}
+                          />
+                        </div>
                       </div>
                     </div>
                   ))}
