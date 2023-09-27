9b6999887a48f545ad9a12b27203d2b585bcb23e
Huzaifa353 <58498320+Huzaifa353@users.noreply.github.com>
2023-09-27 07:31:44 -0700

    SSL Secured API Link Updation

--- src/config/env.js
+++ src/config/env.js
@@ -1,6 +1,6 @@
 
 const global = {
-    apiURL: 'http://lsqcondoofficial.com/torontocondomania/public/',
+    apiURL: 'https://lsqcondoofficial.com/torontocondomania/public/',
     mapboxAccessToken: 'pk.eyJ1IjoiaHV6YWlmYTUzIiwiYSI6ImNsbXJmOW1iOTA3Nm4ybHFtN2V0bHV0dG8ifQ.9a5LJmvzUyGGCH1Av-TKbA',
   };
 
