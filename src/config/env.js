586369cc283e80771e28fcbd5cbcd8b46022e66e
Huzaifa353 <58498320+Huzaifa353@users.noreply.github.com>
2023-10-23 11:59:16 +0500

    Update env.js

--- src/config/env.js
+++ src/config/env.js
@@ -1,6 +1,5 @@
 
 const global = {
-    // apiURL: 'https://lsqcondoofficial.com/torontocondomania/public/',
     apiURL: 'http://lsqcondoofficial.com/torontocondomania/public/',
     mapboxAccessToken: 'pk.eyJ1IjoiaHV6YWlmYTUzIiwiYSI6ImNsbXJmOW1iOTA3Nm4ybHFtN2V0bHV0dG8ifQ.9a5LJmvzUyGGCH1Av-TKbA',
   };
